1bfa3a980a5b771908b860a03f08e0f0f5866941
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-14 14:10:43 +0530

    added post and get integration

--- client/src/component/ShortUrl/ShortUrl.js
+++ client/src/component/ShortUrl/ShortUrl.js
@@ -1,46 +1,68 @@
-import React from 'react';
+import React, { useState,useEffect } from "react";
 import "./ShortUrl.css";
+import axios from "axios";
 
 function ShortUrl() {
+  const [url, setUrl] = useState("");
+
+  const [links, setLinks] = useState([]);
+
+  const loadTask = async () => {
+    const { data } = await axios.get("/shorturls");
+    setLinks(data?.data);
+  };
+  useEffect(() => {
+    loadTask();
+  }, []);
+
+  const addUrl = async () => {
+    const { data } = await axios.post("/shortUrls", {
+      url: url,
+    });
+    alert(data?.message);
+    setUrl("");
+  };
   return (
     <>
-    <div className="container">
-      <div className="card shadow-lg mt-5 p-5">
-        <div className="text-center mb-4">
-          <h2>ShortUrl</h2>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="card shadow-sm p-3 mt-5">
-              <form>
-                <h4 className="text-center mt-3 mb-4">Add Full Url</h4>
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Enter FullUrl"
-                  />
-                </div>
-
-                <button
-                  className="btn btn-warning w-100 mb-3"
-                  type="button"
-                >
-                  {" "}
-                  <b>AddUrl</b>
-                </button>
-              </form>
-            </div>
+      <div className="container">
+        <div className="card shadow-lg mt-5 p-5">
+          <div className="text-center mb-4">
+            <h2>ShortUrl</h2>
           </div>
-          <div className="col-md-6">
-            <div className="url-container shadow-sm p-3 mt-2">
-              <h4 className="text-center mt-2 mb-4">All Urls📃</h4>
-              {/* {tasks.map((task) => {
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card shadow-sm p-3 mt-5">
+                <form>
+                  <h4 className="text-center mt-3 mb-4">Add Full Url</h4>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter FullUrl"
+                      value={url}
+                      onChange={(e) => {
+                        setUrl(e.target.value);
+                      }}
+                    />
+                  </div>
+
+                  <button className="btn btn-warning w-100 mb-3" type="button"
+                  onClick={addUrl}>
+                    {" "}
+                    <b>AddUrl</b>
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="col-md-8">
+              <div className="url-container shadow-sm p-3 mt-2">
+                <h4 className="text-center mt-2 mb-4">All Urls📃</h4>
+                {links.map((link) => {
                 return (
                   <div className="card shadow-sm p-1 mt-2">
-                    <h5>{task?.title}</h5>
-                    <p>{task?.description}</p>
-                    <span
+                    <h5>{link?.url}</h5>
+                    {/* <p>{link?.short}</p> */}
+                    {/* <span
                       className="delete-button"
                       onClick={() => {
                         deleteTask(task?._id);
@@ -48,17 +70,17 @@ function ShortUrl() {
                     >
                       {" "}
                       ❌
-                    </span>
+                    </span> */}
                   </div>
                 );
-              })} */}
+              })}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
 
-export default ShortUrl
+export default ShortUrl;
